d87594b62784b42006136c58d6fc24a6c8d58ed4
kucmoving <92262463+kucmoving@users.noreply.github.com>
2021-10-14 18:24:36 +0800

    Update 5. random.py

--- 5. random.py
+++ 5. random.py
@@ -25,3 +25,10 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 random.shuffle(letters)
 print(letters)
 
+#random color
+def random_color()
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
